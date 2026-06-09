6450c50bb151bf7284c28bdf3c163c995b3b946c
AngelMoreno24 <angel.moreno8424@gmail.com>
2026-06-09 02:10:16 -0700

    created basic routes for frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home/Home";/*
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-*/
-
-function Login() {
-  return <h1>Login Page</h1>;
-}
-
-function Dashboard() {
-  return <h1>Dashboard</h1>;
-}
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
