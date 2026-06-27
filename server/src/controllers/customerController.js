af14632910ada9aa8c2ed51c4c56f908206aedac
AngelMoreno24 <angel.moreno8424@gmail.com>
2026-06-26 23:15:57 -0700

    API: create customer endpoint

---
+++ server/src/controllers/customerController.js
@@ -0,0 +1,34 @@
+import prisma from "../lib/prisma.js";
+
+export const createCustomer = async (req, res) => {
+
+    try {
+
+        const { firstName, lastName, email, phone } = req.body;
+
+        const { companyId, role } = req.user;
+
+        if (!companyId || role !== "OWNER") {
+            return res.status(403).json({ error: "Forbidden" });
+        }
+
+        if (!firstName) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+    
+        const user = await prisma.user.create({
+        data: {
+            firstName,
+            lastName,
+            email,
+            phone
+        },
+        });
+
+        return res.status(201).json(user);
+    }catch (error) { 
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+
+}
